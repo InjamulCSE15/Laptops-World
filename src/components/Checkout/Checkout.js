3a51c04da9a0002d216d991a11d7c4ab91796530
InjamulCSE15 <yeasin.mohammod018@gmail.com>
2021-04-08 02:46:19 +0600

    testing site

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,18 +1,60 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
-import {UserContext} from '../../App';
+import { UserContext } from '../../App';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+    MuiPickersUtilsProvider,
+    KeyboardDatePicker,
+} from '@material-ui/pickers';
+
+import './Checkout.css'
+import { Button } from '@material-ui/core';
 
 const Checkout = () => {
     const { id } = useParams();
 
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const [selectedDate, setSelectedDate] = useState(new Date());
+
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+    };
+
     const [product, setProduct] = useState([])
-        useEffect(()=>{
-            fetch(`https://mysterious-sea-38335.herokuapp.com/checkout/${id}`)
+    useEffect(() => {
+        fetch(`https://mysterious-sea-38335.herokuapp.com/checkout/${id}`)
             .then(response => response.json())
-            .then(data =>setProduct(data))
-        },[])
+            .then(data => setProduct(data))
+    }, [])
+
+    const handleCheckout = () => {
+        const orderedDate = document.getElementById('date').value;
+        const userName = loggedInUser.name;
+        const itemCount = document.getElementById('quantity').value;
+
+        console.log(orderedDate);
+        const detailsOrdered = {
+            name: product.name,
+            details: product.details,
+            price: product.price,
+            quantity: itemCount,
+            email: loggedInUser.email,
+            user: userName,
+            date: orderedDate
+        }
+        console.log(detailsOrdered);
+        fetch('http://localhost:5000/checkoutItems', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(detailsOrdered)
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+            })
+    }
 
     return (
         <div>
@@ -22,8 +64,9 @@ const Checkout = () => {
                         <tr>
                             <th><h4>Your checkout items:</h4></th>
                             <th style={{ width: '10%' }}>Quantity</th>
-                            <th style={{ width: '15%' }} className="text-center">Price</th>
-                            <th style={{ width: '10%' }}></th>
+                            <th style={{ width: '10%' }} className="text-center">Price</th>
+                            <th style={{ width: '10%' }} ></th>
+                            <th style={{ width: '10%' }} >Customize</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -37,9 +80,10 @@ const Checkout = () => {
                                 </div>
                             </td>
                             <td data-th="Quantity">
-                                <input type="number" className="form-control text-center" value="1" />
+                                <input type="number" className="form-control text-center" id="quantity" value="1" />
                             </td>
-                            <td data-th="Price" className="text-center">{product.price}</td>
+                            <td data-th="Price" className="text-center">{product.price} BDT</td>
+                            <td className="text-center"></td>
                             <td className="actions text-end" data-th="">
                                 <button className="btn btn-info btn-sm" title="Edit"><i className="fa fa-edit"></i></button> <button className="btn btn-danger btn-sm" title="Delete"><i className="fa fa-trash-o"></i></button>
                             </td>
@@ -48,9 +92,34 @@ const Checkout = () => {
                     <tfoot>
                         <tr>
                             <td><Link to="/home" className="btn btn-warning"><i className="fa fa-angle-left"></i> Continue Shopping</Link></td>
-                            <td  className="hidden-xs"></td>
-                            <td className="hidden-xs text-center"><strong>Total: {product.price}</strong></td>
-                            <td><Link to="/order" className="btn btn-success btn-block">Checkout <i className="fa fa-angle-right"></i></Link></td>
+                            <td className="hidden-xs"></td>
+                            <td className="hidden-xs text-center"><strong>Total: {product.price} BDT</strong></td>
+                            <td className="hidden-xs"></td>
+                            <td><MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                <Grid container justify="space-around">
+
+                                    <KeyboardDatePicker
+                                        margin="normal"
+                                        id="date"
+                                        label="Date(dd/MM/yyyy)"
+                                        format="dd/MM/yyyy"
+                                        value={selectedDate}
+                                        onChange={handleDateChange}
+                                        KeyboardButtonProps={{
+                                            'aria-label': 'change date',
+                                        }}
+                                    />
+                                </Grid>
+                                <div>
+                                    <button className="btn btn-outline-success" onClick={handleCheckout}>
+
+                                        <Link to="/order">Checkout <i className="fa fa-angle-right"></i> </Link>
+
+                                    </button>
+                                </div>
+
+                            </MuiPickersUtilsProvider>
+                            </td>
                         </tr>
                     </tfoot>
                 </table>
--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Order = () => {
+    const [items, setItems] = useState([]);
+    useEffect(() => {
+        fetch('https://mysterious-sea-38335.herokuapp.com/orderedItems')
+            .then(response => response.json())
+            .then(data => setItems(data));
+    }, [])
     return (
-        <div>
-            <h3>Your Ordered Items:</h3>
+        <div className="container mt-3">
+            <h3>You ordered {items.length}items:</h3>
+            
+            <table className="table mt-3">
+                <thead className="table-dark">
+                    <tr>
+                        <th>Products</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        items.map(item =>
+                            <tr>
+                                <td>{item.name}</td>
+                                <td>{item.quantity}</td>
+                                <td>{item.price}</td>
+                            </tr> 
+                        ) 
+                    }
+                </tbody>
+                <tfoot>
+                    <tr>
+                    <td><Link to="/home" className="btn btn-warning"><i className="fa fa-angle-left"></i> Continue Shopping</Link></td>
+                                <td><strong>Total Products:</strong> {items.length}</td>
+                                <td ><strong>Total Price:</strong> ##### BDT</td>
+                        </tr>
+                </tfoot>
+            </table>
         </div>
     );
 };
